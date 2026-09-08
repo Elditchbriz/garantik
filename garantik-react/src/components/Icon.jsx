1a46895a15d20b6ef67242a80d8923e08f55a580
Elditchbriz <elditchbriz@gmail.com>
2026-09-08 20:06:16 +0200

    Delete garantik-react/src/components/Icon.jsx

--- garantik-react/src/components/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Icon({ name, className = '', style = {} }) {
-  return (
-    <svg className={`icon ${className}`} style={style}>
-      <use href={`#ic-${name}`} xlinkHref={`#ic-${name}`} />
-    </svg>
-  );
-}
